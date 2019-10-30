7bbd6c85ee4c98eb1837011bde32b7e5798605b7
anonimak <sanonimak@gmail.com>
2019-10-30 10:28:03 +0700

    v1

--- test/test.php
+++ test/test.php
@@ -1,12 +1,23 @@
 <?php 
-require_once "../Conn.php";
-require_once "../backup.php";
+require_once "./database/Conn.php";
+require_once "../helpers/backup.php";
 
+// buat koneksi ke database
 $conn = new Conn;
 $pdo = $conn->db;
 
+// lokasi file yg dibackup dan nama filenya
 $filePath = '/database/backup/'.time().'.sql';
-$tables = ['districts'];
+
+// spesifik tabel yang akan di backup
+// 
+// 
+// untuk detail tabel bisa spt ini
+// $tables = ['nama_table1', 'nama_table2','nama_tableN'];
+// 
+// 
+// untuk semua table
+$tables = ['*'];
 
 backupDatabaseTables($pdo, $tables, $filePath);
 
